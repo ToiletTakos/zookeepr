f53391263527c7028529953fc55b06283ca64f3f
Dan <dan.pandolfo@gmail.com>
2021-10-27 12:19:53 -0400

    api can handle both get and post requests as well as validate info

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
 const { animals } = require('./data/animals.json');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//parse incoming string or array data
+app.use(express.urlencoded({ extended:true }));
+//pasre incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -50,6 +58,43 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+
+    //writeFileSync is synchronous version of writeFile and doesnt require a callback function
+    fs.writeFileSync(
+        //path.join to join the value of _dirname(directoryname) which represents the directory of the file we executed the code in,
+        //with the path to animals.json
+        path.join(__dirname, './data/animals.json'),
+        //save the JavaScript array data as JSON
+        // null means we dont want to edit and of our existing data
+        //2 indicates we want to create white space between our values so its more readable
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+
+    //return finished code to post route for response
+    return animal;
+}
+
+function validateAnimal(animal) {
+    // checks to make sure something was entered || checks to make sure what was entered was a string
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    // checks ti make sure something was entered || checks to make sure what was entered was an array
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -68,6 +113,22 @@ app.get('/api/animals/:id', (req, res) => {
     }
 });
 
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    //if any data in req.body is incorrect, send a 400 error back
+    if (!validateAnimal(req.body)) {
+        // send a http status code and a message explaining what went wrong
+        res.status(400).send('The animal is not properly formatted.');
+    }
+    else {
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+});
+
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!S`);
+    console.log(`API server now on port ${PORT}!`);
 });
